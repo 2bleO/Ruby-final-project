733f1512154343ee3281c447d097f8367560d765
onesis <Onesis.olea@gmail.com>
2021-11-09 19:53:35 -0400

    Implement can_be_archived?() && move_to_archive() methods
    move_to_archive() in the Item class
    
    should reuse can_be_archived?() method
    
    should change the archived property to true if the result of the can_be_archived?() method is true
    
    should do nothing if the result of the can_be_archived?() method is false

--- item.rb
+++ item.rb
@@ -21,10 +21,12 @@ class Item
   end
 
   def move_to_archive
+    @archived = can_be_archived?
   end
 
   private
 
   def can_be_archived?
+    (Time.now.year - @publish_date.year) > 10
   end
 end
