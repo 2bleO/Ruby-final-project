4823f9d732f342bf5ad3e1268d79076363d63128
onesis <Onesis.olea@gmail.com>
2021-11-09 19:51:10 -0400

    Test: can_be_archived? method to return true or false
    should return true if published_date is older than 10 years  otherwise, it should return false

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe Item do
+  before :each do
+    @item = Item.new(Time.now)
+  end
+
+  describe '#can_be_archived?' do
+    it 'should return true if published_date is older than 10 years' do
+      @item = Item.new(Time.new('2010/10/10'))
+      @item.move_to_archive
+      expect(@item.archived).to be_truthy
+    end
+
+    it 'should return false if published_date is less than 10 years' do
+      @item.move_to_archive
+      expect(@item.archived).to eql false
+    end
+  end
 end
