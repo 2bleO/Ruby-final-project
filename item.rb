5c8d06ed3043d8ec3832d3b2365627c68681ec52
onesis <Onesis.olea@gmail.com>
2021-11-09 19:33:24 -0400

    Define Item class properties && custom setter methods
    All Item class properties visible in the diagram should be defined and set up in the constructor method.
    
    Exception: properties for the 1-to-many relationships should NOT be set in the constructor method. Instead, they should have a custom setter method created.

--- item.rb
+++ item.rb
@@ -1,2 +1,30 @@
 class Item
+  attr_reader :genre, :author, :game, :label, :archived
+  attr_accessor :id
+
+  def initialize(publish_date)
+    @id = Random.rand(1000..10_000)
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def add_genre(genre)
+  end
+
+  def add_author()
+  end
+
+  def add_label()
+  end
+
+  def add_game()
+  end
+
+  def move_to_archive
+  end
+
+  private
+
+  def can_be_archived?
+  end
 end
