90793b057eb70f9196e4f919ae2e8575a7ee4c6c
onesis <Onesis.olea@gmail.com>
2021-11-10 20:29:11 -0400

    Test: Book class
    can_be_archived?() in the Book class should override the method from the parent class.
    
    should return true if parent's method returns true OR if cover_state equals to "bad" otherwise, it should return false

---
+++ spec/book_spec.rb
@@ -0,0 +1,33 @@
+require_relative 'spec_helper'
+
+describe Book do
+  before :each do
+    @book1 = Book.new Time.new('2009/05/10'), 'alfaguara', 'good'
+    @book2 = Book.new Time.now, 'alfaguara', 'bad'
+    @book3 = Book.new Time.now, 'alfaguara', 'good'
+  end
+
+  describe '#book' do
+    it 'returns a Book object' do
+      expect(@book1).to be_an_instance_of(Book)
+    end
+  end
+
+  describe '#can_be_archived?' do
+    context 'when 10 years old or cover is bad' do
+      it 'can be archived' do
+        expect(@book1.can_be_archived?).to be_truthy
+      end
+
+      it 'can be archived' do
+        expect(@book2.can_be_archived?).to be_truthy
+      end
+    end
+
+    context 'when less than 10 years and cover is good' do
+      it 'cannot be archived' do
+        expect(@book3.can_be_archived?).to be_falsey
+      end
+    end
+  end
+end
